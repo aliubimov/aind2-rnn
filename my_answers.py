2eb1f2138784b90fcbf1b66e55f09a0b986cd0f5
Andrii Liubimov <andrii.liubimov@gmail.com>
2017-08-30 15:49:37 +0000

    Solution: seq generation

--- my_answers.py
+++ my_answers.py
@@ -6,16 +6,16 @@ from keras.layers import LSTM
 import keras
 
 
-# TODO: fill out the function below that transforms the input series 
+# fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
-    X = []
-    y = []
+    X = [series[x:x + window_size] for x in range(len(series) - window_size)]
+    y = series[window_size:]
 
     # reshape each 
     X = np.asarray(X)
-    X.shape = (np.shape(X)[0:2])
+    #X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
 
@@ -23,12 +23,18 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+    for x in punctuation:
+        text = text.replace(x, '')
 
     return text
 
@@ -38,9 +44,18 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
+    for x in range(0, len(text) - window_size, step_size):
+        inputs.append(text[x:x+window_size])
+        outputs.append(text[x + window_size])
+
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size,num_chars)))
+    model.add(Dense(num_chars))
+    model.add(Dense(num_chars, activation='softmax'))
+
+    return model
